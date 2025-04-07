9cf5a71bbe12f93689bbe0046561abb1b0864562
Jeremy Kurtz <Jt3cho@gmail.com>
2025-04-06 23:33:39 -0400

    Update Algo.py

--- Algo.py
+++ Algo.py
@@ -1,9 +1,9 @@
-# define the trading time
-# download pytz by typing "pip install pytz" in command line
+# Define the trading time
 from datettime import datetime, time
+# Download pytz by typing "pip install pytz" in command line
 import pytz
 
-# define trading day tells us if it is between the hours of 0930 and 1545 Eastern standard that the Algo will be trading
+# Define trading day tells us if it is between the hours of 0930 and 1545 Eastern standard that the Algo will be trading
 def is_trading_time():
   # Define time for New York Stock Exchange (Eastern Time)
   eastern = pytz.timezone('US/Eastern')
@@ -15,14 +15,17 @@ def is_trading_time():
   market_open = time(9, 30) #0930
   market_close = time(15,45) #1545
 
-# Check if now is within trading hours
+# Checks if the time is in the Trading hours or outside of trading hours, the function will return TRUE or FALSE
   return market_open <= now <= market_close
 
-# Example use: 
+# Ensures that the code is only run when we execute the file directly not when its imported into another file
 if __name__ == "__main__":
+  # If statement calls back to line 7 when we defined trading time as Eastern Time
   if is_trading_time():
-    print("Market is Open - STARTED.")
+    # This print statement sends a tpyed message into our command line that tells us that the market is Open
+    print("Market is Open - STARTED")
+    # If the time isn't in the selected hours than the script will print "Market is CLOSED - STOPPED" 
   else:
-    print("Market is CLOSED - STOPPED.")
+    print("Market is CLOSED - STOPPED")
 
 
